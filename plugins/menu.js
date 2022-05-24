01a2644b13079d47beb73d6328c27435da4be595
NanaCans <106129602+NanaCans@users.noreply.github.com>
2022-05-24 09:55:49 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -13,7 +13,7 @@ var totalram = `${Math.round(os.totalmem / 1024 / 1024)}`
 var ramDipake = totalram-sisaram
 const defaultMenu = {
     before: `
-╭──『 _*BOTCAHX*_ 』──⬣
+╭──『 _*NanaBotz*_ 』──⬣
 │⬡ *Hai*, %name!
 │
 │⬡ *Tersisa* : %limit Limit
@@ -29,8 +29,6 @@ const defaultMenu = {
 │⬡ *Uptime* : %uptime
 │⬡ *Database* : %rtotalreg dari %totalreg
 │⬡ *Memory Used* : ${ramDipake}MB / ${totalram}MB
-│⬡ *Instagram* :
-│⬡ https://instagram.com/mursid.st
 ╰───────────⬣`.trimStart(),
     header: '╭──〘 %category 〙──⬣',
     body: '│⬡%cmd %islimit %isPremium',
@@ -243,7 +241,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 │⬡ *Chat Terbanned* : ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}
 │⬡ *Pengguna Terbanned* : ${Object.entries(global.db.data.users).filter(user => user[1].banned).length}
 ╰───────────⬣`.trim(),
-                        "footerText": "© _*BOTCAHX*_",
+                        "footerText": "© _*NanaBotz*_",
                         "buttonText": "*Klik Disini*",
                         "listType": "SINGLE_SELECT",
                         "sections": [
@@ -392,7 +390,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
                             }, {
                                 "rows": [{
                                     "title": "Owner bot",
-                                    "description": "Pemilik Mursid S",
+                                    "description": "Pemilik Nana Cans",
                                     "rowId": ".owner"
                                 }, {
                                     "title": "Donasi",
@@ -496,8 +494,8 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
         }
         text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-await conn.fakeReply(m.chat, 'Loading...', '0@s.whatsapp.net', 'BY MURSID (+6288233832771)', 'status@broadcast')
- conn.send3ButtonLoc(m.chat, await (await fetch(image)).buffer(), text.trim(), `*Runtime* *${uptime}*\n*${week} ${date}*\n*Created By* @6288233832771⁩`, 'ρємιℓιк вσт', '.owner', 'ɪɴƒᴏ мυʀѕι∂', '.infomursid', 'ʀυℓєѕ', '.rules', m, { contextInfo: {
+await conn.fakeReply(m.chat, 'Loading...', '0@s.whatsapp.net', 'BY NANA (6281348568830)', 'status@broadcast')
+ conn.send3ButtonLoc(m.chat, await (await fetch(image)).buffer(), text.trim(), `*Runtime* *${uptime}*\n*${week} ${date}*\n*Created By* NanaCans`, 'ρємιℓιк вσт', '.owner', 'ɪɴƒᴏ ηαηα', '.infonana', '∂σηαѕι', '.donasi', m, { contextInfo: {
 mentionedJid: ['6288233832771@s.whatsapp.net']
 }
 })
