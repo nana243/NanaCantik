ebc32e1aa3d38df2548f652066e2bdb136d95283
NanaCans <106129602+NanaCans@users.noreply.github.com>
2022-05-24 10:02:01 +0700

    Update and rename infomursid.js to infonana.js

--- plugins/infonana.js
+++ plugins/infonana.js
@@ -1,28 +1,26 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *INFORMASI OWNER* ´ˎ˗
-│ ✎ _Nama_ : Mursid S
-│ ✎ _Sekolah_ : SMKN 2 Wonosari
-│ ✎ _Umur_ : 17
-│ ✎ _Asal_ : Yogyakarta
-│ ✎ _Status_ : Pelajar SMK
+│ ✎ _Nama_ : Nana Saputri
+│ ✎ _Pendidikan_ : Kerja
+│ ✎ _Umur_ : 21
+│ ✎ _Asal_ : -
+│ ✎ _Status_ : Single
 │ ✎ _Official Grup 1_ :
 │    https://chat.whatsapp.com/LPFQ2X1cnihB0fb8F8cZau
 │ ✎ _Official Grup 2_ :
 │    https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
 │ ✎ _Official Grup 3_ :
 │    https://chat.whatsapp.com/HcGcIB09sIvKrGytO8yfFn
-│ ✎ _Instagram_ : 
-│    instagram.com/mursid.st
 │ ✎ _WhatsApp_ :
-│    wa.me/6288233832771
-╰───────────────────
+│    wa.me/6281348568830
+╰──────────────────
 `.trim(), m)
 }
 
-handler.help = ['infomursid']
+handler.help = ['infonana']
 handler.tags = ['main', 'utama']
-handler.command = /^(infomursid)$/i
+handler.command = /^(infonana)$/i
 
 handler.exp = 150
 
