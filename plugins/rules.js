1000a4f389e1c8ed73749d9e5623edd58a41da18
nana243 <106389343+nana243@users.noreply.github.com>
2022-05-30 19:19:10 +0700

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -4,15 +4,15 @@ let handler = async(m, { conn }) => {
     let teks = `Kebijakan Privasi, Syarat Ketentuan dan Peraturan Mursid BOT
 
 Kebijakan Privasi
-1. MursidBot tidak akan merekam data riwayat chat user.
-2. MursidBot tidak akan menyebarkan nomor users.
-3. MursidBot tidak akan menyimpan media yang dikirimkan oleh users.
-4. MursidBot tidak akan menyalah gunakan data data users.
-5. Owner Mursid berhak melihat data riwayat chat users.
-6. Owner Mursid berhak melihat status users.
-7. Owner Mursid dapat melihat riwayat chat, dan media yang dikirimkan users.
+1. NanaBotz tidak akan merekam data riwayat chat user.
+2. NanaBotz tidak akan menyebarkan nomor users.
+3. NanaBotz tidak akan menyimpan media yang dikirimkan oleh users.
+4. NanaBotz tidak akan menyalah gunakan data data users.
+5. Owner Queen Nana berhak melihat data riwayat chat users.
+6. Owner Queen Nana berhak melihat status users.
+7. Queen Nana dapat melihat riwayat chat, dan media yang dikirimkan users.
 
-Peraturan MursidBot
+Peraturan NANA BOT
 1. Users dilarang menelpon maupun memvideo call nomor bot.
 2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
 3. Users diharap tidak melakukan spam dalam penggunaan bot.
@@ -22,12 +22,12 @@ Peraturan MursidBot
 
 Syarat Ketentuan MursidBot
 1. Bot akan keluar dari group apabila sudah waktunya keluar.
-2. MursidBot dapat mem-ban users secara sepihak terlepas dari users salah atau tidak.
-3. MursidBot *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
-4. MursidBot akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
-5. MursidBot bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+2. NanaBotz dapat mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. NanaBotz *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
+4. NanaBotz akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. Queen Nana  bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
 
-SC : Thanks to
+Alhamdulillah : Thanks to
 -Allah SWT
 -Nurutomo
 -Ariffb
